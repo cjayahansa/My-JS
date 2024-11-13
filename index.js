81c62eecb8e13ef7af64f8a866f2f982e8791dcf
cjayahansa <chaminduliyangama1761@gmail.com>
2024-11-13 13:20:07 +0530

    my fist js project

---
+++ index.js
@@ -0,0 +1,41 @@
+//this is my first java script project
+console.log('hello world');
+
+//variables
+let name ='chamindu';
+console.log(name);
+// cannot be a reserved keyword
+// shoud be a meaning full
+// cannot a start with a nummber (1name)
+//cannot  contain a space or hyphen(-)
+// are case - sensitive
+let firstName ='cj',lastName='jayahansa';
+console.log(firstName,lastName);
+
+//consetnt
+const eage=13;
+console.log(eage); 
+
+//primitive type
+let name2 ='cj2';
+let age=40;
+let isApproved=false;
+let firstName2=undefined;
+let color=null;
+//change data type data moddels depend 
+
+let person={
+    name3: 'cj4',
+    age:40,
+};
+//dot natation
+person.name3='cj5';
+console.log(person.name3);
+
+//bracket notation
+person['name3']='mary';
+console.log(person.name3);
+//
+let selection='name3';
+person[selection]='cj6';
+console.log(person.name3);
